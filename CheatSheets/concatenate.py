190dee3b1acf2adbab72cc374298da6f4841928f
Dale Woodard <dale@wingedw.com>
2021-09-17 16:59:12 -0700

    Added 3 Concatenate ways

---
+++ CheatSheets/concatenate.py
@@ -0,0 +1,13 @@
+"""
+Which one do you use?
+Which one is easiest to read?
+Which one is most flexible?
+"""
+name = 'Pathrise'
+
+# 1
+print( name + " rocks!")
+# 2
+print("{} rocks!".format(name))
+# 3
+print(f"{name} rocks!")
