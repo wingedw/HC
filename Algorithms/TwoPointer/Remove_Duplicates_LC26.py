5fffc82ad2f150789186116d5dc50dae8751120a
Dale Woodard <dale@wingedw.com>
2021-11-29 22:49:58 -0800

    Leet Code 26 remove duplicates from sorted array

---
+++ Algorithms/TwoPointer/Remove_Duplicates_LC26.py
@@ -0,0 +1,32 @@
+'''
+Given an integer array nums sorted in non-decreasing order, 
+remove the duplicates in-place such that each unique element 
+appears only once. The relative order of the elements should 
+be kept the same.
+
+Since it is impossible to change the length of the array in 
+some languages, you must instead have the result be placed 
+in the first part of the array nums. More formally, if there 
+are k elements after removing the duplicates, then the first 
+k elements of nums should hold the final result. It does not 
+matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots 
+of nums.
+
+Do not allocate extra space for another array. You must do 
+this by modifying the input array in-place with O(1) extra 
+memory.'''
+
+def removeDuplicates(nums):
+    _length = 1
+    if len(nums)==0:
+        return 0
+    for i in range(1,len(nums)):
+        if nums[i] != nums[i-1]:
+            nums[_length] = nums[i]
+            _length +=1
+    return _length 
+
+nums = [1,1,2]
+print(removeDuplicates(nums))
